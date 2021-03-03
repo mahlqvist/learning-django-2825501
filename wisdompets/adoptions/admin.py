3e118f31cd6afbbb1f1ed3d929132852c6d660da
Thomas <mahlqvist@msn.com>
2021-03-03 20:23:07 +0100

    creating migrations

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
-from .models import Pet
-
-@admin.register(Pet)
-class PetAdmin(admin.ModelAdmin):
-    list_display = ['name', 'species', 'breed', 'age', 'sex']
+# Register your models here.
